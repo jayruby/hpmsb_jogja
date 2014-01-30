3f0914f2cad8c875693ed0324eacecf317a479ef
agos jayadi <jayadigoes@gmail.com>
2014-01-30 20:25:16 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HpmsbJogja::Application.config.secret_key_base = 'ff7f62528098c298a2b3f14023e0f6d102d3989bd41324f410daa5af2027801acea121daed8ac647f5bb25c7e0a5d96a0ef82d21014676e5f31897f11fad2b64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HpmsbJogja::Application.config.session_store :cookie_store, key: '_hpmsb_jogja_session'
